321cc9ac4d6d845fe57facf30e3e6ab2f2e11d71
Stuart Chalk <schalk@unf.edu>
2022-06-17 17:05:42 -0400

    Code mostly working.  Need to fix a bug or two...

---
+++ ingestion/ingest.py
@@ -0,0 +1,128 @@
+# code to take a tsv file and create a model definition in the models.py file
+# assumptions:
+#     all fields should be strings
+#     define type of char field based on longest entry
+#     add primary key field commensurate with size of tsv files (rows)
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django.settings")
+django.setup()
+
+from Django.settings import *
+from functions import *
+from ingestion import models
+import subprocess
+
+# define variables
+filepath = '/Biochemistry/Aliases/Unique_ModelSEED_Reaction_Aliases.tsv'
+tablename = 'reaction_aliases'
+delimiter = '\t'
+modelname = tablename.lower().replace('_', '').capitalize()
+datastore = {}
+labels = []
+
+# read file and process (add data to the datastore variable)
+fp = open(str(BASE_DIR) + filepath)
+content = fp.read()
+for line in content.splitlines():
+    fields = line.split(delimiter)
+    # initialize datastore dictionary
+    if not datastore:
+        for field in fields:
+            label = field.replace(" ", "").lower()
+            datastore.update({label: []})  # removes spaces from field names
+            labels.append(label)
+        continue
+    # populate fields
+    for idx, value in enumerate(fields):
+        datastore[labels[idx]].append(value)
+
+# analyze fields for longest entry
+maxlens = []
+cnt = 0
+for key, values in datastore.items():
+    if cnt == 0:
+        cnt = len(values)
+    maxlen = len(str(max(values, key=len)))
+    maxlens.append(maxlen)
+
+# check that model is already in models.py
+filepath = "models.py"
+classstr = "class " + modelname
+if not findclass(filepath, classstr):
+    # create model text
+    mtext = ['\n\nclass ' + modelname + '(models.Model):\n',
+             '    """ getting data from the ' + tablename + ' table """\n',
+             '    id = models.AutoField(primary_key=True)\n']
+    for idx, label in enumerate(labels):
+        flen = 0
+        if maxlens[idx] < 17:
+            flen = 16
+        elif maxlens[idx] < 65:
+            flen = 64
+        elif maxlens[idx] < 257:
+            flen = 256
+        elif maxlens[idx] < 1025:
+            flen = 1025
+        else:
+            flen = 4096
+        mtext.append('    ' + label + ' = models.CharField(max_length=' + str(flen) + ', default=\'\')\n')
+    mtext.append('    comments = models.CharField(max_length=256, default=\'\')\n')
+    mtext.append('    updated = models.DateTimeField(auto_now=True)\n')
+    mtext.append('\n')
+    mtext.append('    class Meta:\n')
+    mtext.append('        managed = True\n')  # this is important as it authorizes Django to add the database
+    mtext.append('        db_table = \'' + tablename + '\'\n')
+
+    # add lines to the ingestion models.py
+    mfile = open("../ingestion/models.py", "a")  # append mode
+    mfile.writelines(mtext)
+    mfile.close()
+    mfile = open("../ingestion/models.py", "r")
+    print(mfile.read())
+    mfile.close()
+
+    # run makemigrations
+    subprocess.call(["python", "../manage.py", "makemigrations", "ingestion"], stdout=None)
+    print('Class created and migrations made')
+else:
+    print('Class already created')
+
+# check if table is already in database
+out = subprocess.check_output("python ../manage.py inspectdb", shell=True)
+if classstr not in out.decode('utf-8'):  # it's output as a byte string
+    print("Adding table to DB")
+    # run migrate
+    out = subprocess.call(["python", "../manage.py", "migrate", "ingestion"], stdout=None)
+    print(out)
+    exit()
+    if out:  # zero means process ran OK
+        print("error running migrate")
+        subprocess.call(["python", "../manage.py", "migrate", "ingestion"])
+        exit()
+else:
+    print('DB already created')
+    # empty table
+    cleartable(tablename)
+
+# add data to table
+count = 0
+for key, values in datastore.items():
+    for idx, v in enumerate(values):
+        row = getattr(models, modelname)()
+        # fill table
+        for field in labels:
+            setattr(row, field, datastore[field][idx])
+        row.save()
+        count += 1
+        print(row)
+    break
+print('Complete: added ' + str(count) + ' rows')
+
+# reset files to the starting state
+os.remove('migrations/0001_initial.py')
+with open('models.py', 'w') as myfile:
+    myfile.write('from django.db import models\n')
+myfile.close()
+exit()
