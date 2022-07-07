5026466e824f5e1890584e8c982f82c95ce0fc32
meganm0828 <n01529009@unf.edu>
2022-07-07 14:37:15 -0400

    Code for ingestion of iupac names and inchikeys from Pubchem Rest API

---
+++ compoundsapp/modelseeddb.py
@@ -0,0 +1,41 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class CompoundsMm(models.Model):
+    compoundid = models.CharField(max_length=16)
+    abbreviation = models.TextField()
+    name = models.TextField()
+    iupac_name = models.TextField()
+    formula = models.CharField(max_length=64)
+    mass = models.CharField(max_length=16)
+    source = models.CharField(max_length=64)
+    inchikey = models.CharField(max_length=64)
+    charge = models.CharField(max_length=16)
+    key_charge = models.CharField(max_length=16)
+    charge_ok = models.CharField(max_length=16)
+    is_core = models.CharField(max_length=16)
+    is_obsolete = models.CharField(max_length=16)
+    linked_compound = models.CharField(max_length=64)
+    is_cofactor = models.CharField(max_length=16)
+    deltag = models.CharField(max_length=16)
+    deltagerr = models.CharField(max_length=16)
+    pka = models.CharField(max_length=256)
+    pkb = models.CharField(max_length=256)
+    abstract_compound = models.CharField(max_length=16)
+    comprised_of = models.CharField(max_length=16)
+    aliases = models.TextField()
+    smiles = models.TextField()
+    notes = models.CharField(max_length=16)
+    comments = models.CharField(max_length=256)
+    updated = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'compounds_mm'
---
+++ compoundsapp/testinput.py
@@ -0,0 +1,74 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django.settings")
+django.setup()
+
+from django.db.migrations.recorder import MigrationRecorder
+from Django.settings import *
+from compoundsapp.modelseeddb import *
+from compoundsapp.testfunctions import *
+import subprocess
+import importlib
+import requests
+
+x = 1
+
+# compoundsdata = CompoundsMm.objects.all()
+# for compound in compoundsdata.iterator():
+# for compound in CompoundsMm.objects.all():
+#     x = 0
+#     ickey = str(CompoundsMm.objects.values_list('inchikey')[x])
+#     ichkey = ickey[2]
+#     print(ickey)
+#     x = x+1
+    # text = requests.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/' + ickey + '/property/IUPACname/txt')
+    # unfiltered = str(text.content)
+    # iupacName = filterText(unfiltered)
+    # cpd = CompoundsMm.objects.get(inchikey = ickey)
+    # cpd.iupac_name = iupacName
+    # cpd.save()
+
+# input for iupac_name -> got to id 12320 before timeout / lost connection, 41 with 503 error
+cpdset = CompoundsMm.objects.all()
+for cpd in cpdset.iterator():
+    ickey = str(cpd.inchikey)
+    text = requests.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/' + ickey + '/property/IUPACname/txt')
+    unfiltered = str(text.content)
+    iupacName = filterText(unfiltered)
+    cpd.iupac_name = iupacName
+    cpd.save()
+    print('Added ' + str(x) + ' rows')
+    x = x + 1
+
+#24652 to start, either blank or with 503 error, 11886 blank before resolving 503 (53 errors), 11898 after
+# cpdset = CompoundsMm.objects.all()
+# for cpd in cpdset.iterator():
+#     iup = cpd.iupac_name
+#     if('503' in iup):
+#         ickey = str(cpd.inchikey)
+#         text = requests.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/' + ickey + '/property/IUPACname/txt')
+#         unfiltered = str(text.content)
+#         iupacName = filterText(unfiltered)
+#         cpd.iupac_name = iupacName
+#         cpd.save()
+#         # print(str(cpd.name))
+#         print('Added ' + str(x) + ' rows')
+#         x = x + 1
+
+# input for 9739 compounds with no listed inchikey, need to remove 404 errors and replace /n with ;
+# cpdset = CompoundsMm.objects.all()
+# for cpd in cpdset.iterator():
+#     ickey = str(cpd.inchikey)
+#     cpdname = str(cpd.name)
+#     if(ickey == ""):
+#     # if('503' in ickey):
+#         text = requests.get('https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/' + cpdname + '/property/InChIKey/txt')
+#         unfiltered = str(text.content)
+#         inchik = filterSeperateText(unfiltered)
+#         # print(inchik)
+#         cpd.inchikey = inchik
+#         cpd.save()
+#         print('Added ' + str(x) + ' rows')
+#         x = x + 1
+
